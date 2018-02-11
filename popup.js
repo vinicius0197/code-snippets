1c902ffa815602136ec35c98c8f2065c4da427fb
Vinicius <viniciuscosta0197@gmail.com>
2018-02-11 13:28:31 -0200

    Added automatic resize to text area

--- popup.js
+++ popup.js
@@ -1,23 +1,44 @@
 function pasteSelection() {
-	// chrome.runtime.sendMessage( {method: "getSelection"}, function(response) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var observe;
+		if (window.attachEvent) {
+			observe = function (element, event, handler) {
+				element.attachEvent('on'+event, handler);
+			};
+		}
+		else {
+			observe = function (element, event, handler) {
+				element.addEventListener(event, handler, false);
+			};
+		}	
 		var currTab = tabs[0];
 		var tab_id = currTab.id;
+		
 		chrome.tabs.sendMessage(tab_id, {method: "getSelection"}, function(response) {
 			var text = document.getElementById('text');
+			function resize () {
+				text.style.height = 'auto';
+				text.style.height = text.scrollHeight+'px';
+			}
+			function delayedResize () {
+				window.setTimeout(resize, 0);
+			}
 			text.innerHTML = response.data;
+			
+			observe(text, 'change', resize);
+			observe(text, 'cut', delayedResize);
+			observe(text, 'paste', delayedResize);
+			observe(text, 'drop', delayedResize);
+			observe(text, 'keydown', delayedResize);
+
+			text.focus();
+			text.select();
+			resize();
 		});
 	});
 }
 
-	//chrome.tabs.sendMessage(
-	//	var text = document.getElementById('text');
-	//	text.innerHTML = response.data;
-
-		
-
 document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById("myButton").addEventListener("click", pasteSelection);
 });
-
 
--- selection.js
+++ selection.js
@@ -1,12 +1,8 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	console.log('Event triggered');
 	if (message.method == "getSelection") {
 		sendResponse({data: window.getSelection().toString()});
-		console.log('Everythin okay!');
-	}
+		}
 	else {
 		sendResponse( {} );
-		console.log('Maybe not so much...');
-	}
-	//return true;
+		}
 });
